4ebf239ac06ffbfb662f1cc33c0fb3be756d3ae9
zlatanjakic <zlatan.jakic@klika.ba>
2024-06-23 17:14:19 +0200

    Added comparison for the date of the flight

--- flight_plan_test_0.pl
+++ flight_plan_test_0.pl
@@ -1,6 +1,6 @@
 % Define the flight facts: flight(ID, Type, ScheduledTime, FlightNumber, PriorityStatus).
 flight(2, departure, date(2024, 6, 22), time(8, 0), 'FL100', scheduled).
-flight(3, arrival, date(2024, 6, 22), time(8, 10), 'FL200', scheduled).
+flight(3, arrival, date(2024, 6, 21), time(8, 0), 'FL200', scheduled).
 flight(1, departure, date(2024, 6, 22), time(8, 5), 'FL101', scheduled).
 flight(4, arrival, date(2024, 6, 22), time(8, 15), 'FL201', scheduled).
 flight(5, departure, date(2024, 6, 22), time(8, 20), 'FL102', scheduled).
@@ -15,17 +15,24 @@ flight(13, departure, date(2024, 6, 22), time(9, 0), 'FL106', scheduled).
 flight(14, arrival, date(2024, 6, 22), time(9, 5), 'FL206', scheduled).
 flight(15, departure, date(2024, 6, 22), time(9, 10), 'FL107', scheduled).
 
+% Pravilo za ispisivanje datuma
+display_date(date(Year, Month, Day)) :-
+    format('~d-~|~`0t~d~2+-~|~`0t~d~2+~n', [Year, Month, Day]).
 
-time_difference(time(H1, M1), time(H2, M2), Diff) :-
-    Minutes1 is H1 * 60 + M1,
-    Minutes2 is H2 * 60 + M2,
+time_to_minutes(time(H, M), Minutes) :-
+    Minutes is H * 60 + M.
+
+time_difference(Time1, Time2, Diff) :-
+    time_to_minutes(Time1, Minutes1),
+    time_to_minutes(Time2, Minutes2),
     Diff is abs(Minutes1 - Minutes2).
 
 % Rule to check if two flights conflict.
 conflict(Flight1, Flight2) :-
-    flight(Flight1, Type1, _, Time1, _, _),
-    flight(Flight2, Type2, _, Time2, _, _),
+    flight(Flight1, Type1, Date1, Time1, _, _),
+    flight(Flight2, Type2, Date2, Time2, _, _),
     Type1 = Type2,
+    Date1 = Date2,
     time_difference(Time1, Time2, Diff),
     Diff < 5.
 
@@ -35,7 +42,6 @@ find_conflicts(Flight, Conflicts) :-
 
 % Rule to generate a flight plan without conflicts.
 generate_flight_plan([], []).
-
 generate_flight_plan([Flight|Rest], [Flight|Plan]) :-
     find_conflicts(Flight, Conflicts),
     Conflicts = [],
@@ -48,10 +54,16 @@ generate_flight_plan([Flight|Rest], Plan) :-
 
 % Rule to sort flights by scheduled time.
 sort_flights_by_time(Flights, SortedFlights) :-
-    predsort(compare_flights_by_time, Flights, SortedFlights).
+    predsort(compare_flights_by_dateTime, Flights, SortedFlights).
 
-compare_flights_by_time(Order, flight(ID1, Type1, Date1, Time1, FlightNumber1, PriorityStatus1), flight(ID2, Type2, Date2, Time2, FlightNumber2, PriorityStatus2)) :-
-    compare(Order, Time1, Time2).
+compare_flights_by_dateTime(Order, flight(ID1, Type1, Date1, Time1, FlightNumber1, PriorityStatus1), flight(ID2, Type2, Date2, Time2, FlightNumber2, PriorityStatus2)) :-
+    (Time1 @< Time2 -> Order = '<';
+    Time1 @> Time2 -> Order = '>';
+    Date1 @< Date2 -> Order = '<';
+    Date1 @> Date2 -> Order = '>';
+    ID1 @< ID2 -> Order = '<';
+    ID1 @> ID2 -> Order = '>';
+    Order = '=').
 
 % Rule to create the final flight plan.
 create_flight_plan(Plan) :-
