e9be24dadb55d096eb093f6276a8e5977672b17d
zlatanjakic <zlatan.jakic@klika.ba>
2024-06-24 16:06:54 +0200

    Restructured the code for readability

--- flight_plan_test_0.pl
+++ flight_plan_test_0.pl
@@ -1,3 +1,4 @@
+% --------------------------------------------FACTS--------------------------------------------
 % Define the flight facts: flight(ID, Type, ScheduledTime, FlightNumber, PriorityStatus).
 flight(2, departure, date(2024, 6, 22), time(8, 5), 'FL100', priority).
 flight(3, departure, date(2024, 6, 22), time(8, 5), 'FL200', scheduled).
@@ -15,37 +16,8 @@ flight(13, departure, date(2024, 6, 21), time(8, 5), 'FL106', scheduled).
 flight(14, arrival, date(2024, 6, 22), time(9, 5), 'FL206', scheduled).
 flight(15, departure, date(2024, 6, 21), time(8, 5), 'FL107', scheduled).
 
-% Pravilo za ispisivanje datuma
-display_date(date(Year, Month, Day)) :-
-    format('~d-~|~`0t~d~2+-~|~`0t~d~2+~n', [Year, Month, Day]).
 
-% Pravilo za racunanje minuta
-time_to_minutes(time(H, M), Minutes) :-
-    Minutes is H * 60 + M.
-
-% Pravilo za racunanje razlike izmedju dva vremena u minutama
-time_difference(Time1, Time2, Diff) :-
-    time_to_minutes(Time1, Minutes1),
-    time_to_minutes(Time2, Minutes2),
-    Diff is abs(Minutes1 - Minutes2).
-
-% Pravilo za provjeru konflikta
-conflict(FlightID1, FlightID2) :-
-    flight(FlightID1, Type1, Date1, Time1, _, Status1),
-    flight(FlightID2, Type2, Date2, Time2, _, Status2),
-
-    Type1 = Type2,
-    Date1 = Date2,
-    time_difference(Time1, Time2, Diff),
-    Diff < 5,
-    (Status1 = Status2 ; (Status1 = scheduled, Status2 = priority)). % Konflik je samo ako je status letova isti ili drugi let ima prioritet
-
-% Pravilo za pronalazenje svih konflikta sa trenutnim letom
-find_conflicts(Flight, Conflicts) :-
-    findall(Conflict, (flight(Conflict, _, _, _, _, _), conflict(Flight, Conflict), Conflict \= Flight), Conflicts).
-
-
-%-------------------------------------------generate_flight_plan WITH CONFLICTS-----------------------------------
+% --------------------------------------------PLAN GENERATION--------------------------------------------
 
 % Rekurzivno pravilo za generisanje plana i liste konflikta
 generate_flight_plan([], [], []).
@@ -58,8 +30,6 @@ generate_flight_plan([flight(FlightID, Type, Date, Time, FlightNumber, Status)|R
     find_conflicts(FlightID, Conflicts),
     Conflicts \= [],
     generate_flight_plan(Rest, Plan, ConflictIDs).
-
-%------------------------------------------------------------------------------------------------------------------
 
 
 % Pravilo za poredjenje dva leta po kriteriju datum > vrijeme > ID
@@ -78,6 +48,26 @@ create_flight_plan(Plan, ConflictIDs) :-
     predsort(compare_flights, Flights, SortedFlights),
     generate_flight_plan(SortedFlights, Plan, ConflictIDs).
 
+
+% --------------------------------------------CONFLICT DETECTION--------------------------------------------
+
+% Pravilo za provjeru konflikta
+conflict(FlightID1, FlightID2) :-
+    flight(FlightID1, Type1, Date1, Time1, _, Status1),
+    flight(FlightID2, Type2, Date2, Time2, _, Status2),
+
+    Type1 = Type2,
+    Date1 = Date2,
+    time_difference(Time1, Time2, Diff),
+    Diff < 5,
+    (Status1 = Status2 ; (Status1 = scheduled, Status2 = priority)). % Konflik je samo ako je status letova isti ili drugi let ima prioritet
+
+% Pravilo za pronalazenje svih konflikta sa trenutnim letom
+find_conflicts(Flight, Conflicts) :-
+    findall(Conflict, (flight(Conflict, _, _, _, _, _), conflict(Flight, Conflict), Conflict \= Flight), Conflicts).
+
+% --------------------------------------------DISPLAY FUNCTIONS--------------------------------------------
+
 % Pravilo za ispisivanje jednog leta
 print_flight(flight(ID, Type, date(Year, Month, Day), time(Hour, Minute), FlightNumber, Status)) :-
     format('Flight ID: ~|~t~d~2+, Type: ~|~t~w~9+, Date: ~d-~|~`0t~d~2+-~|~`0t~d~2+, Time: ~|~`0t~d~2+:~|~`0t~d~2+, Flight Number: ~w, Status: ~w~n', [ID, Type, Year, Month, Day, Hour, Minute, FlightNumber, Status]).
@@ -87,6 +77,19 @@ print_flights([]).
 print_flights([Flight | Rest]) :-
     print_flight(Flight),
     print_flights(Rest).
+
+% Pravilo za ispisivanje letova iz liste IDeva
+print_flights_from_id(FlightIDs) :-
+    get_flights_from_ids(FlightIDs, Flights),
+    print_flights(Flights).
+
+% Pravilo za ispisivanje datuma (UNUSED)
+display_date(date(Year, Month, Day)) :-
+    format('~d-~|~`0t~d~2+-~|~`0t~d~2+~n', [Year, Month, Day]).
+
+
+% --------------------------------------------HELPER FUNTIONS--------------------------------------------
+
 
 % Pravilo za pronalazenje leta preko IDa
 find_flight_by_id(FlightID, Flight) :-
@@ -99,10 +102,18 @@ get_flights_from_ids([FlightID|RestIDs], [Flight|RestFlights]) :-
     find_flight_by_id(FlightID, Flight),
     get_flights_from_ids(RestIDs, RestFlights).
 
-% Pravilo za ispisivanje letova iz liste IDeva
-print_flights_from_id(FlightIDs) :-
-    get_flights_from_ids(FlightIDs, Flights),
-    print_flights(Flights).
+% Pravilo za racunanje minuta
+time_to_minutes(time(H, M), Minutes) :-
+    Minutes is H * 60 + M.
+
+% Pravilo za racunanje razlike izmedju dva vremena u minutama
+time_difference(Time1, Time2, Diff) :-
+    time_to_minutes(Time1, Minutes1),
+    time_to_minutes(Time2, Minutes2),
+    Diff is abs(Minutes1 - Minutes2).
+
+
+% --------------------------------------------MAIN PROGRAM--------------------------------------------
 
 % Pravilo za ispisivanje citavog plana letova i pronadjenih konflikta
 display_flight_plan() :-
